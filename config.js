0beb0a5b2d42a82f5469fe880a3589b2862506d3
numero15 <nathanaeltardif@gmail.com>
2019-05-03 14:37:42 +0200

    first commit

---
+++ config.js
@@ -0,0 +1,8 @@
+var config = {
+  consumer_key:         'InCdnFfM9LwbWTnir6qfaktsC',
+  consumer_secret:      '00yIDB24QDw7c7EdbiPx4LQ300YdumipJofNmigqdFzpJTAfHn',
+  access_token:         '1124255789651247114-vhzRs2h9bwa6qeE1Z1W7wZX0DqW0I1',
+  access_token_secret:  'WWjnFLKxoV9bhc89EF8NUfHXBBQ7EByoX0AdDLFSUQM4K'
+}
+
+module.exports = config;
---
+++ server.js
@@ -0,0 +1,55 @@
+var fs = require('fs'),
+    path = require('path'),
+    Twit = require('twit'),
+    config = require(path.join(__dirname, 'config.js'));
+
+//Accès vers twitter
+var T = new Twit(config);
+
+
+
+//Setup communication
+var http = require("http");
+var url = require("url");
+var path = require("path");
+var ServerIP = '127.0.0.1',
+    port = '8080';
+
+var Server = http.createServer(
+	function (request , response) {
+    	console.log("Request Recieved");
+    	request.on('data', function (chunk) {
+		    console.log('GOT DATA! ' + chunk);
+		    tweet(chunk.toString());
+		});
+
+		// request.on('end', function () {
+	 //        console.log("end");
+	 //    });
+
+	 //    response.writeHead(200, {'Content-Type': 'text/html'});
+  //   	response.end('callback(\'{\"msg\": \"OK\"}\')');
+    }
+); 
+
+Server.listen(port, ServerIP, function () {
+    console.log("Listening..." + ServerIP + ":" + port);
+});
+
+
+
+
+//Tweet functions
+function tweet(contenu) {
+	console.log("tweeting");
+	var params = {
+		status: contenu
+	};
+	T.post(
+		'statuses/update', 
+		params, 
+		function(err, data, response) {
+		    console.log(data)
+		}
+	);
+}
